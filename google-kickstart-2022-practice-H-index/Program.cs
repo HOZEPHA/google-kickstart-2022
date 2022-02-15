b78dda925bbd06c619d55117458b0b7889e47eca
Zephecy <hozepha.abdoulaly@outlook.com>
2022-02-15 11:55:59 +0100

    #task02 : new project created

---
+++ google-kickstart-2022-practice-H-index/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int cou = Convert.ToInt32(Console.ReadLine());
+			for (int i = 0; i < cou; i++)
+			{
+				string result = $"Case #{i + 1}: ";
+
+				int N = int.Parse(Console.ReadLine());
+
+				var current = 0;
+
+				var input = Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToList();
+
+                for (int j = 0; j < N; j++)
+                {
+					if(input[j]>= 1)
+                    {
+						current++;
+                    }
+                }
+
+
+
+
+
+
+
+			}
+		}
+	}
+}
